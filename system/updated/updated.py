b6a88c8de3768b8677f0a9a8466955d58acb659f
Fmstrat <nospam@nowsci.com>
2025-09-03 13:09:48 +0000

    update timing

--- system/updated/updated.py
+++ system/updated/updated.py
@@ -31,8 +31,10 @@ FINALIZED = os.path.join(STAGING_ROOT, "finalized")
 
 OVERLAY_INIT = Path(os.path.join(BASEDIR, ".overlay_init"))
 
-DAYS_NO_CONNECTIVITY_MAX = 14     # do not allow to engage after this many days
-DAYS_NO_CONNECTIVITY_PROMPT = 10  # send an offroad prompt after this many days
+# DAYS_NO_CONNECTIVITY_MAX = 14     # do not allow to engage after this many days
+# DAYS_NO_CONNECTIVITY_PROMPT = 10  # send an offroad prompt after this many days
+DAYS_NO_CONNECTIVITY_MAX = 99999     # do not allow to engage after this many days
+DAYS_NO_CONNECTIVITY_PROMPT = 99999  # send an offroad prompt after this many days
 
 class UserRequest:
   NONE = 0
